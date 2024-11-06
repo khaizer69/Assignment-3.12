0d97eeebdd5111bf63175dc48c52b0ebf874753b
Dijay Kumar <dijaykumar@gmail.com>
2024-11-06 20:46:43 +0800

    Add Lambda function and Terraform deployment

--- main.tf
+++ main.tf
@@ -22,9 +22,6 @@ resource "aws_iam_role" "lambda_role" {
 resource "aws_lambda_function" "my_lambda" {
   function_name = "DijayLambdaFunction"
   
-  s3_bucket = aws_s3_bucket.lambda_bucket.bucket
-  s3_key    = "lambda/my_lambda_function.zip"
-  
   handler = "lambda_function.lambda_handler"
   runtime = "python3.10"
   
@@ -35,15 +32,4 @@ resource "aws_lambda_function" "my_lambda" {
       ENV_VAR = "value"  # Optional, you can define environment variables for Lambda
     }
   }
-}
-
-resource "aws_s3_bucket" "lambda_bucket" {
-  bucket = "Dijay-lambda-deployment-bucket-12345"  # Change to a globally unique name
-}
-
-resource "aws_s3_bucket_object" "lambda_object" {
-  bucket = aws_s3_bucket.lambda_bucket.bucket
-  key    = "lambda/my_lambda_function.zip"
-  source = "lambda/my_lambda_function.zip"  # Local path to your zipped Lambda code
-  acl    = "private"
 }
